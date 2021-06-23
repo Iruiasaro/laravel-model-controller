9e9531f966f8471bad713ba7bcd726b539dee130
Iruiasaro <brunettirosario@gmail.com>
2021-06-23 10:20:46 +0200

    inserimento file css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/styles.css">
     <title>Home</title>
 </head>
 <body>
-        @foreach($movies as $movie)
 
+<div class="wrapper">
+
+    <div class="film">
+        
+        @foreach($movies as $movie)
             <h2>{{ $movie->title }}</h2>
-            
             <ul>
-                <li>Title: {{ $movie->original_title }}</li>
+                <li>Original Title: {{ $movie->original_title }}</li>
+                <li>Nationality: {{$movie->nationality}}</li>
                 <li>Date: {{ $movie->date }}</li>
                 <li>Vote: {{ $movie->vote }}</li>
             </ul>
-            
         @endforeach
+
+    </div>
+</div>        
     
 </body>
 </html>
